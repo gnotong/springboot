d3b958025cb37083eaa08db17acfde374179a0d0
lineDjuissi <linedjuissi@yahoo.com>
2022-08-12 04:52:07 +0200

    SpringBoot: Activity#2 random fortune

---
+++ src/com/notgabs/demo/RandomFortuneService.java
@@ -0,0 +1,24 @@
+package com.notgabs.demo;
+
+import java.util.Random;
+
+public class RandomFortuneService implements FortuneService {
+
+	private String[] fortunes = {
+			"Don’t hold onto things that require a tight grip.",
+			"I didn’t come this far to only come this far.",
+			"Look how far you've come.",
+	};
+	
+	private Random random = new Random();
+	
+	@Override
+	public String getFortune() {
+		int index = random.nextInt(fortunes.length);
+		
+		String theFortune = fortunes[index];
+		
+		return theFortune;
+	}
+
+}
