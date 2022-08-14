c29380acf4f1da06ca1537282d043bac4e280612
lineDjuissi <linedjuissi@yahoo.com>
2022-08-15 00:59:46 +0200

    SpringBoot: annotation bean scope prototype

--- src/com/notgabs/annotations/TenisCoach.java
+++ src/com/notgabs/annotations/TenisCoach.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("singleton")
+@Scope("prototype")
 public class TenisCoach implements Coach {
 
 	@Autowired
